784f42463410e40ee830ffd0814023e5d4d63862
derrickLouis <djlouis012005@gmail.com>
2024-06-24 16:59:48 -0700

    Adding writing email list to csv

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import json
-import os
+from django.http import HttpResponse
 
 def main():
-    with open("user_data.json", "r") as users_file:
+    with open("user_data.json") as users_file:
         users = json.load(users_file)
     
-    with open("jobs_data.json", "r") as jobs_file:
+    with open("jobs_data.json") as jobs_file:
         jobs = json.load(jobs_file)
     
-    with open("course_data.json", "r") as courses_file:
+    with open("course_data.json") as courses_file:
         courses = json.load(courses_file)
-
     
+    outfile = open("eList.csv", "w")
+
+    emailList = []
+
+    def email_gather(request):
+        emailList.append(request)
+        return
+    
+    if len(emailList) >= 1:
+        for email in emailList:
+            outfile.write(email)
 
 
 if __name__ == "__main__":
