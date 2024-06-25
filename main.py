9f42a0039df6ac11d4ca811761012fcec1438caf
derrickLouis <djlouis012005@gmail.com>
2024-06-24 21:38:14 -0700

    Implemented auto-email function

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 import json
 from django.http import HttpResponse
+import smtplib, ssl
+
 
 def main():
-    with open("user_data.json") as users_file:
-        users = json.load(users_file)
+    emailList = [] #Holds single email
+
+    with open("user_data.json") as users_file: 
+        users = json.load(users_file) #Saves user json as dict
     
     with open("jobs_data.json") as jobs_file:
-        jobs = json.load(jobs_file)
+        jobs = json.load(jobs_file) #Saves jobs json as dict
     
     with open("course_data.json") as courses_file:
-        courses = json.load(courses_file)
+        courses = json.load(courses_file) #Saves courses json as dict
     
-    outfile = open("eList.csv", "w")
-
-    emailList = []
-
-    def email_gather(request):
-        emailList.append(request)
+    def email_gather(request): #View for email input from site
+        emailList.append(request) #saves request to emailList
         return
+
+    if len(emailList) > 0:
+        email = emailList[0]
+    else:
+        email = None
     
-    if len(emailList) >= 1:
-        for email in emailList:
-            outfile.write(email)
+    if email:
+        port = 587  # For starttls
+        smtp_server = "smtp.gmail.com"
+        sender_email = "linkedinhackathon30@gmail.com"
+        password = "hackathon123"
+        receiver_email = email
+        message = """This is your internship Message"""
+        context = ssl.create_default_context()
+        with smtplib.SMTP(smtp_server, port) as server: #Opens SMTP server to send email
+            server.starttls(context=context)
+            server.login(sender_email, password) #Logs into sender email
+            server.sendmail(sender_email, receiver_email, message) #Sends message
 
 
 if __name__ == "__main__":
